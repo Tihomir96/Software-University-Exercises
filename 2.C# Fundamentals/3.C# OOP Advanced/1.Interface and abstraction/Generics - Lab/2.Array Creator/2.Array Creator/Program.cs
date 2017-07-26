e9a4c4a8a86b8e9ca40c6fbec907a89378ceb2cd
Tihomir96 <tihomirkirilov96@gmail.com>
2017-07-26 21:20:09 +0300

    Add files via upload

---
+++ 2.C# Fundamentals/3.C# OOP Advanced/1.Interface and abstraction/Generics - Lab/2.Array Creator/2.Array Creator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] strings = ArrayCreator.Create(5, "pesho");
+        int[] integers = ArrayCreator.Create(10, 33);
+
+
+    }
+}
