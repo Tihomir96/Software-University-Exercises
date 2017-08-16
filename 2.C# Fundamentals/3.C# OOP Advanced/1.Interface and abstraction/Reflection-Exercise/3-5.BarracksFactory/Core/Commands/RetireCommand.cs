45260dc9ba7157751b56b9451c72e7328922a9db
Tihomir96 <tihomirkirilov96@gmail.com>
2017-08-16 22:38:25 +0300

    Reflection

---
+++ 2.C# Fundamentals/3.C# OOP Advanced/1.Interface and abstraction/Reflection-Exercise/3-5.BarracksFactory/Core/Commands/RetireCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03BarracksFactory.Attributes;
+using _03BarracksFactory.Contracts;
+
+namespace _03BarracksFactory.Core.Commands
+{
+    public class RetireCommand:Command
+    {
+        [Inject]
+        private IRepository repository;
+
+        public RetireCommand(string[] data) : base(data)
+        {
+        }
+
+        public override string Execute()
+        {
+            this.repository.RemoveUnit(this.Data[1]);
+            return $"{this.Data[1]} retired!";
+            
+        }
+    }
+}
---
+++ 2.C# Fundamentals/3.C# OOP Advanced/1.Interface and abstraction/Reflection-Exercise/3-5.BarracksFactory/Core/Factories/UnitFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace _03BarracksFactory.Core.Factories
+{
+    using System;
+    using Contracts;
+
+    public class UnitFactory : IUnitFactory
+    {
+        private const string unitNamespace = "_03BarracksFactory.Models.Units.";
+        public IUnit CreateUnit(string unitType)
+        {
+            Type uniType = Type.GetType(unitNamespace+unitType);
+            IUnit unitInstance = (IUnit)Activator.CreateInstance(uniType);
+            return unitInstance;
+        }
+    }
+}
